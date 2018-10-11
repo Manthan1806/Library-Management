299ee30a10cedc64baf47b03fd9d24feec96049b
Manthan1806 <32006356+Manthan1806@users.noreply.github.com>
2018-10-11 09:16:17 +0530

    Add files via upload

--- IssueRequest.java
+++ IssueRequest.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
@@ -12,36 +13,35 @@ public class IssueRequest {
 	public IssueRequest(Connection con)throws SQLException
 	{
 		Statement stmt = con.createStatement();
-		String s = "create table if not exists issue_request(id varchar(15),name varchar(20),book varchar(20))";
+		String s = "create table if not exists issue_request(id varchar(15),book varchar(20))";
 		stmt.executeUpdate(s);
 	}
 	
-	public void sendEmail(String b)
+	public void sendEmail(String b,Connection con)throws SQLException
 	{
-		String s="sidhdarth37@gmail.com";
-		
-	      String to = s;//change accordingly
-	      String from = "pednekar.manthan18@gmail.com";//change accordingly
-	      String host = "localhost";//or IP address
-
-	     //Get the session object
-	      Properties properties = System.getProperties();
-	      properties.setProperty("mail.smtp.host", host);
-	      Session session = Session.getDefaultInstance(properties);
-
-	     //compose the message
-	      try{
-	         MimeMessage message = new MimeMessage(session);
-	         message.setFrom(new InternetAddress(from));
-	         message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
-	         message.setSubject("Book Availablity");
-	         message.setText("The book you have requested is available now");
-	         
-	         // Send message
-	         Transport.send(message);
-	         System.out.println("message sent successfully....");
-
-	      }catch (MessagingException mex) {mex.printStackTrace();}
-
+		Statement stmt1 = con.createStatement();
+		String s1 = "select * from issue_request where book = '"+b+"';";
+		ResultSet rs = stmt1.executeQuery(s1);
+		String id = "";
+		if(rs.next())
+		{
+			id = rs.getString(1);
+			System.out.println(rs.getString(1));
+		}
+		Statement stmt2 = con.createStatement();
+		String s2 = "select email from StudentInfo where id = '"+id+"';";
+		ResultSet rs2 = stmt2.executeQuery(s2);
+		if(rs2.next())
+		{
+			library_management.Address = rs2.getString(1);
+			System.out.println(rs2.getString(1));
+		}
+	}
+	
+	public void issue(Connection con,String id,String b)throws SQLException
+	{
+		Statement stmt1 = con.createStatement();
+		String s = "insert into issue_request values('"+id+"','"+b+"');";
+		stmt1.executeUpdate(s);
 	}
 }
